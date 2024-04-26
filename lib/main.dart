fe4ecab96c471c949e510fecddd3b82fe2fad9e9
Cheikh Oumar DIALLO <88355786+cheikhouma@users.noreply.github.com>
2024-04-26 11:43:56 +0000

    fix the bug

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: HomePage(),
     );
