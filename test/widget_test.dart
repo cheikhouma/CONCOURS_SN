bdf6ed4e5e41ed0ce38750d25446c21a68d801eb
Cheikh Oumar DIALLO <dcheikhoumar@ept.sn>
2024-08-21 18:58:40 +0000

    chnging test folder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ept/main.dart';
+import 'package:CONCOURS_SN/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
