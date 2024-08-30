41e663a4cc88826d29e943132eb0d27123dd2bdb
Cheikh Oumar DIALLO <dcheikhoumar@ept.sn>
2024-08-30 11:35:02 +0000

    Last version

--- lib/viewer/pdf_viewer.dart
+++ lib/viewer/pdf_viewer.dart
@@ -1,10 +1,6 @@
 import 'package:Concours_SN/main_pages/constantes.dart';
 import 'package:flutter/material.dart';
 import 'package:pdfx/pdfx.dart';
-import 'package:path_provider/path_provider.dart';
-import 'dart:io';
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:permission_handler/permission_handler.dart'; // Import pour les permissions
 
 class PdfViewPage extends StatefulWidget {
   final String pdfFileName;
@@ -31,87 +27,6 @@ class _PdfViewPageState extends State<PdfViewPage> {
   void dispose() {
     _pdfController.dispose();
     super.dispose();
-  }
-
-  Future<void> _requestPermissionAndDownload() async {
-    // Vérification du statut de la permission
-    var status = await Permission.storage.status;
-
-    if (status.isGranted) {
-      // Téléchargez le fichier si la permission est déjà accordée
-      await _downloadFile();
-    } else if (status.isDenied ||
-        status.isRestricted ||
-        status.isLimited ||
-        status.isPermanentlyDenied) {
-      // Demander la permission à l'utilisateur
-      bool? permissionGranted = await showDialog<bool>(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Permission nécessaire'),
-          content: Text(
-              'Cette application a besoin de la permission de stockage pour télécharger le fichier.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(false); // Refuser la permission
-              },
-              child: Text('Annuler'),
-            ),
-            TextButton(
-              onPressed: () async {
-                Navigator.of(context).pop(true); // Accepter la permission
-              },
-              child: Text('Autoriser'),
-            ),
-          ],
-        ),
-      );
-
-      if (permissionGranted == true) {
-        status = await Permission.storage.request();
-
-        if (status.isGranted) {
-          await _downloadFile(); // Téléchargez le fichier si la permission est accordée
-        } else {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Permission de stockage refusée')),
-          );
-        }
-      }
-    }
-  }
-
-  Future<void> _downloadFile() async {
-    try {
-      // Obtenir le chemin du répertoire de téléchargement
-      final directory = await getExternalStorageDirectory();
-      final downloadDirectory = Directory('${directory!.path}/Concours SN');
-
-      // Créer le répertoire s'il n'existe pas encore
-      if (!await downloadDirectory.exists()) {
-        await downloadDirectory.create(recursive: true);
-      }
-
-      // Déterminer le chemin complet du fichier
-      final filePath =
-          '${downloadDirectory.path}/${widget.pdfFileName.split('/').last}';
-
-      // Lire le fichier à partir des assets
-      final byteData = await rootBundle.load(widget.pdfFileName);
-      final file = File(filePath);
-
-      // Écrire les données dans un fichier
-      await file.writeAsBytes(byteData.buffer.asUint8List());
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Fichier téléchargé à $filePath')),
-      );
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Erreur lors du téléchargement du fichier: $e')),
-      );
-    }
   }
 
   @override
@@ -146,11 +61,6 @@ class _PdfViewPageState extends State<PdfViewPage> {
       ),
       bottomNavigationBar: const BottomAppBar(
         color: white,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _requestPermissionAndDownload, // Appeler la nouvelle méthode
-        backgroundColor: principale_color,
-        child: const Icon(Icons.download, color: white),
       ),
     );
   }
