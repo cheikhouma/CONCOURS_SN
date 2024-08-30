89f664a254b9e39c5582481fda07c2f30aa98bfb
Cheikh Oumar DIALLO <dcheikhoumar@ept.sn>
2024-08-30 10:54:28 +0000

    index on main: 9d4c938 Structures des bd

--- lib/main_pages/constantes.dart
+++ lib/main_pages/constantes.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 // ignore: constant_identifier_names
 const Color principale_color = Color.fromRGBO(127, 23, 52, 1);
 
+const Color secondary_color = Color.fromRGBO(217, 217, 217, 1);
+
 const Color white = Colors.white;
 
 const Color black = Colors.black;
--- lib/viewer/pdf_viewer.dart
+++ lib/viewer/pdf_viewer.dart
@@ -1,6 +1,10 @@
 import 'package:Concours_SN/main_pages/constantes.dart';
 import 'package:flutter/material.dart';
 import 'package:pdfx/pdfx.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:permission_handler/permission_handler.dart'; // Import pour les permissions
 
 class PdfViewPage extends StatefulWidget {
   final String pdfFileName;
@@ -13,11 +17,11 @@ class PdfViewPage extends StatefulWidget {
 
 class _PdfViewPageState extends State<PdfViewPage> {
   late PdfController _pdfController;
+  bool hasRequestedPermission = false;
 
   @override
   void initState() {
     super.initState();
-    // Initialize the PdfController with the PDF file passed from the HomePage
     _pdfController = PdfController(
       document: PdfDocument.openAsset(widget.pdfFileName),
     );
@@ -29,40 +33,124 @@ class _PdfViewPageState extends State<PdfViewPage> {
     super.dispose();
   }
 
+  Future<void> _requestPermissionAndDownload() async {
+    // Vérification du statut de la permission
+    var status = await Permission.storage.status;
+
+    if (status.isGranted) {
+      // Téléchargez le fichier si la permission est déjà accordée
+      await _downloadFile();
+    } else if (status.isDenied ||
+        status.isRestricted ||
+        status.isLimited ||
+        status.isPermanentlyDenied) {
+      // Demander la permission à l'utilisateur
+      bool? permissionGranted = await showDialog<bool>(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Permission nécessaire'),
+          content: Text(
+              'Cette application a besoin de la permission de stockage pour télécharger le fichier.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false); // Refuser la permission
+              },
+              child: Text('Annuler'),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.of(context).pop(true); // Accepter la permission
+              },
+              child: Text('Autoriser'),
+            ),
+          ],
+        ),
+      );
+
+      if (permissionGranted == true) {
+        status = await Permission.storage.request();
+
+        if (status.isGranted) {
+          await _downloadFile(); // Téléchargez le fichier si la permission est accordée
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Permission de stockage refusée')),
+          );
+        }
+      }
+    }
+  }
+
+  Future<void> _downloadFile() async {
+    try {
+      // Obtenir le chemin du répertoire de téléchargement
+      final directory = await getExternalStorageDirectory();
+      final downloadDirectory = Directory('${directory!.path}/Concours SN');
+
+      // Créer le répertoire s'il n'existe pas encore
+      if (!await downloadDirectory.exists()) {
+        await downloadDirectory.create(recursive: true);
+      }
+
+      // Déterminer le chemin complet du fichier
+      final filePath =
+          '${downloadDirectory.path}/${widget.pdfFileName.split('/').last}';
+
+      // Lire le fichier à partir des assets
+      final byteData = await rootBundle.load(widget.pdfFileName);
+      final file = File(filePath);
+
+      // Écrire les données dans un fichier
+      await file.writeAsBytes(byteData.buffer.asUint8List());
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Fichier téléchargé à $filePath')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erreur lors du téléchargement du fichier: $e')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Extract the date from the PDF file name
-
+    // Extraire la date du nom de fichier PDF
     RegExp regex = RegExp(r'.*_(.*)\.pdf$');
     RegExpMatch? match = regex.firstMatch(widget.pdfFileName);
-    String date = match!.group(1)!;
+    String date =
+        match?.group(1) ?? 'Inconnu'; // Gérer la valeur null en toute sécurité
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back,
-              color: white), // Change color to black
+          icon: const Icon(Icons.arrow_back, color: white),
           onPressed: () {
-            Navigator.pop(context); // Navigate back to the previous page
+            Navigator.pop(context); // Naviguer vers la page précédente
           },
         ),
         title: Text(
-          'Épreuve  $date',
+          'Épreuve $date',
           style: const TextStyle(
-              fontFamily: "Poppins",
-              fontWeight: FontWeight.w500,
-              color: Colors.white),
+              fontFamily: "Poppins", fontWeight: FontWeight.w500, color: white),
         ),
-        backgroundColor: const Color.fromRGBO(127, 23, 52, 1),
+        backgroundColor: principale_color,
       ),
       body: PdfView(
         controller: _pdfController,
         pageSnapping: false,
         scrollDirection: Axis.vertical,
-        backgroundDecoration: const BoxDecoration(color: Colors.white),
+        backgroundDecoration: const BoxDecoration(color: white),
       ),
       bottomNavigationBar: const BottomAppBar(
         color: white,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _requestPermissionAndDownload, // Appeler la nouvelle méthode
+        backgroundColor: principale_color,
+        child: const Icon(Icons.download, color: white),
       ),
     );
   }
